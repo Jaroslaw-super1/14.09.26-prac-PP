5f9425f3a0600b3826711251f0203f6f8d51fbcb
jar <Jarafan3@gmail.com>
2026-09-16 16:56:20 +0300

    [TASK-1] Add Clicker class

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <chrono>
+
+class Clicker
+{
+public:
+  Clicker():
+  start_(std::chrono::high_resolution_clock::now())
+  {}
+  double millisec() const {
+    using std::chrono::high_resolution_clock;
+    using std::chrono::duration_cast;
+    using std::chrono::milliseconds;
+    auto t = high_resolution_clock::now();
+    return duration_cast< milliseconds >(t - start_).count();
+  }
+private:
+  std::chrono::time_point< std::chrono::steady_clock > start_;
+};
+
+int main(int argc, char ** argv)
+{
+  return 0;
+}
