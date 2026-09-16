556bb6d3c96b8f1f9093480ebebc6e0f51574889
jar <Jarafan3@gmail.com>
2026-09-16 16:59:15 +0300

    [TASK-1] Add main

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <chrono>
+#include <string>
+#include <vector>
 
 class Clicker
 {
@@ -7,6 +9,7 @@ public:
   Clicker():
   start_(std::chrono::high_resolution_clock::now())
   {}
+
   double millisec() const {
     using std::chrono::high_resolution_clock;
     using std::chrono::duration_cast;
@@ -18,7 +21,22 @@ private:
   std::chrono::time_point< std::chrono::steady_clock > start_;
 };
 
+using data_t = std::vector< unsigned long long >;
+using value_t = data_t::value_type;
+
 int main(int argc, char ** argv)
 {
-  return 0;
+  constexpr size_t size{1'000'000'000};
+  double init{0}, total{0};
+  value_t sum{0};
+  {
+    mtt::Clicker cl;
+    data_t values(size, 1);
+    init = cl.millisec();
+    for (size_t i = 0; i < values.size(); ++i)
+    {
+      sum += values[i];
+    }
+    total = cl.millisec();
+  }
 }
