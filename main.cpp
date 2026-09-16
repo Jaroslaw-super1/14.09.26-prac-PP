27ea966c04aa8525c0317bbc3c5ce91bbecceb78
jar <Jarafan3@gmail.com>
2026-09-16 18:06:24 +0300

    [TASK-1] Add sum func

--- main.cpp
+++ main.cpp
@@ -27,16 +27,46 @@ private:
 using data_t = std::vector< unsigned long long >;
 using value_t = data_t::value_type;
 
+
+value_t sum_range(const data_t & data, size_t begin, size_t end)
+{
+  value_t s = 0;
+  for (size_t j = begin; j < end; ++j)
+  {
+    s += data[j];
+  }
+  return s;
+}
+
+
 int main(int argc, char ** argv)
 {
   constexpr size_t size{1'000'000'000};
+
+  const size_t threads = std::stoul(argv[1]);
+
   double init{0}, total{0};
   value_t sum{0};
 
   {
     Clicker cl;
     data_t values(size, 1);
+    size_t v_size = values.size();
+
     init = cl.millisec();
+
+    const std::size_t chunk = v_size / threads;
+    const std::size_t rem   = v_size % threads;
+
+    std::vector< std::future< value_t > > futures;
+    futures.reserve(threads);
+
+
+
+
+
+
+
     for (size_t i = 0; i < values.size(); ++i)
     {
       sum += values[i];
